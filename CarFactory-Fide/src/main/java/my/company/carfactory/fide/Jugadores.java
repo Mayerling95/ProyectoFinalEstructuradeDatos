6521f547a7e95bf6874220f7a74ad491a66a86e0
BritanyCA <105955520+BritanyCA@users.noreply.github.com>
2023-04-01 17:02:32 -0600

    clase jugadores
    Jugadores

---
+++ CarFactory-Fide/src/main/java/my/company/carfactory/fide/Jugadores.java
@@ -0,0 +1,82 @@
+/*
+ * Clase Jugadores
+ */
+package com.mycompany.carfactory.fide;
+
+/*Encargada Britany Campos Aguilar
+ */
+public class Jugadores {
+    //Se establecen 2 jugadores para realizar el juego
+
+    //Atributos
+    private colaOrdenes jugador1Cola;
+    private colaOrdenes jugador2Cola;
+    private int tiempoTranscurrido;
+    private int minutos;
+    private int segundos;
+
+   //Constructor 
+    public Jugadores() {
+        jugador1Cola = new colaOrdenes();
+        jugador2Cola = new colaOrdenes();
+        tiempoTranscurrido = 0;
+        minutos = 0;
+        segundos = 0;
+    }
+
+    //Getters y setters
+    public colaOrdenes getJugador1Cola() {
+        return jugador1Cola;
+    }
+
+    public void setJugador1Cola(colaOrdenes jugador1Cola) {
+        this.jugador1Cola = jugador1Cola;
+    }
+
+    public colaOrdenes getJugador2Cola() {
+        return jugador2Cola;
+    }
+
+    public void setJugador2Cola(colaOrdenes jugador2Cola) {
+        this.jugador2Cola = jugador2Cola;
+    }
+
+    public int getTiempoTranscurrido() {
+        return tiempoTranscurrido;
+    }
+
+    public void setTiempoTranscurrido(int tiempoTranscurrido) {
+        this.tiempoTranscurrido = tiempoTranscurrido;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugadores{" + "jugador1Cola=" + jugador1Cola + ", jugador2Cola=" + jugador2Cola + '}';
+    }
+    
+    
+    
+    
+}
+
+    
+
+    
+    
+  
